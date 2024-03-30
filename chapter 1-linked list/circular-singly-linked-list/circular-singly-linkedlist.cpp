112104396303d2e5658a29405a8bc4f1eb9f2b3a
elianhardyy <elianhardi805@gmail.com>
2024-03-30 17:25:22 +0700

    update: linked list addIndex

--- chapter 1-linked list/circular-singly-linked-list/circular-singly-linkedlist.cpp
+++ chapter 1-linked list/circular-singly-linked-list/circular-singly-linkedlist.cpp
@@ -12,7 +12,7 @@ struct Mahasiswa
 };
 Mahasiswa *head, *tail ,*current, *newNode ;
 // Circular Linked List work
-struct Mahasiswa* circularSingleLinkedList(string data[3])
+struct Mahasiswa* addEmpty(string data[3])
 {
     // initialize node as head
     head = new Mahasiswa();
@@ -27,7 +27,7 @@ struct Mahasiswa* circularSingleLinkedList(string data[3])
 // print circular singly linked list
 void printCircularSinglyLinkedList()
 {
-    current = head;
+    current = head;//new place for print node;
     while(current->next != head){ // when it encounter tail
         cout << current->name << "\t";
         cout << current->nrp << "\t";
@@ -50,15 +50,52 @@ struct Mahasiswa* addFirst(string data[3])
     newNode->next = head;
     //if node has more than one 
     tail->next = newNode;
-    head = newNode; // newNode will link to head 
+    head = newNode; // newNode will link to head or replace head
+}
+// add last in node based on image is after letter "T"
+struct Mahasiswa* addLast(string data[3])
+{
+    newNode = new Mahasiswa();
+    newNode->name = data[0];
+    newNode->nrp = data[1];
+    newNode->jurusan = data[2];
+    tail->next = newNode;
+    newNode->next = head;
+    tail = newNode; // replace tail
+}
+struct Mahasiswa* addIndex(int index, string data[3])
+{
+    newNode = new Mahasiswa();
+    newNode->name = data[0];
+    newNode->nrp = data[1];
+    newNode->jurusan = data[2];
+    newNode->next = NULL;
+    current = head;
+    while(index > 1){
+        current = current->next;
+        index--;
+    }
+    newNode->next = current->next;
+    current->next = newNode;
+    if(current == tail)
+    {
+        tail = tail->next;
+    }
+    current = newNode;
 }
 
 int main()
 {
     string data1[3] = {"Elian","1020040033","PL"};
-    circularSingleLinkedList(data1);
+    addEmpty(data1);
     string data2[3] = {"John","1020040022","OO"};
     addFirst(data2);
+    string data3[3] = {"Nana","1020040022","OO"};
+    addLast(data3);
+    string data4[3] = {"Hala","1020040022","OO"};
+    addLast(data4);
+    string data5[3] = {"Lala","1020040044","11"};
+    addIndex(1,data5);
     printCircularSinglyLinkedList();
 
     
---
+++ chapter 1-linked list/circular-singly-linked-list/tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+string data5[3] = {"Lala","1020040044","00"};
+    addIndex(2,data5);
